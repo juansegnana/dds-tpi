a6134a6324f50a550da389b3becefdda3cbae4bc
Juan <juan.segnana@contractors.scale.com>
2023-05-24 11:57:14 -0300

    script

--- scripts/create_zip.sh
+++ scripts/create_zip.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")/.."
 now=$(date +"%Y-%m-%d")
 
 # Create zip file excluding certain directories and files
-zip -r dd-grupo-6-tpi-$now.zip ./ -x \
+zip -r dds-grupo-6-tpi-$now.zip ./ -x \
 "*.next/*" \
 "*.vscode/*" \
 "*node_modules/*" \
